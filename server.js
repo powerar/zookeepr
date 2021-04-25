390f88544f006677b5c1ea05d2369e38e9070973
powerar <arpower@bell.net>
2021-04-25 15:55:03 -0400

    Add Heroku

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+
+const { animals } = require('./data/animals.json');
+
+const app = express();
+
+// filter the results by query
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults= animalsArray;
+    if (query.personalityTraits) {
+        //Sve personalityTraits as a dedicated array
+        //If personalityTraits is a string, place it into a new array and save.
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        //loop through each trait in the personalityTraits array:
+        personalityTraitsArray.forEach(trait => {
+      // Check the trait against each animal in the filteredResults array.
+      // Remember, it is initially a copy of the animalsArray,
+      // but here we're updating it for each trait in the .forEach() loop.
+      // For each trait being targeted by the filter, the filteredResults
+      // array will then contain only the entries that contain the trait,
+      // so at the end we'll have an array of animals that have every one 
+      // of the traits when the .forEach() loop is finished.
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+
+//get requires two arguments: first is a string that describes the route the client will fetch from, second is callback that will execute every time the route is accessed
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+});
